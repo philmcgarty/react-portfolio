48f465932b26f150ea5d2a4479d9a06e40cf9500
Phil McGarty <philmcgarty@hotmail.com>
2022-08-24 22:22:13 -0400

    added download resume button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,20 @@
 import React from "react";
+import resume from "../assets/resume/Phil McGarty_Web Developer_Resume.pdf"
 // resume page
 function Resume() {
     // function to prevent page reload on button press - button doesn't do anything yet - will allow download of resume when backend added
-    function buttonClick(e) {
-        e.preventDefault();
-        console.log('Download')
-    };
+    // function buttonClick(e) {
+    //     e.preventDefault();
+    //     console.log('Download')
+    // };
     // lists backend and frontend competencies
     return (
         <>
             <h2>Resume</h2>
 
             <div className="row">
-                <div className="col-md-6 col-sm-12">
+                <div className="col-md-3 col-sm-0"></div>
+                <div className="col-md-3 col-sm-12">
                     <h3>Front-end Proficiencies</h3>
                     <ul className="skill-list">
                         <li>HTML</li>
@@ -24,7 +26,7 @@ function Resume() {
                         <li>Handlebars</li>
                     </ul>
                 </div>
-                <div className="col-md-6 col-sm-12">
+                <div className="col-md-3 col-sm-12">
                     <h3>Back-end Proficiencies</h3>
                     <ul className="skill-list">
                         <li>Node.js</li>
@@ -36,10 +38,14 @@ function Resume() {
                         <li>GraphQL</li>
                     </ul>
                 </div>
+                <div className="col-md-3 col-sm-0"></div>
             </div>
 
             {/* resume download button - for future use */}
-            <p><button type="submit" onClick={buttonClick}>Download Resume</button></p>
+            <a href={resume} download="Phil McGarty_Web Developer_Resume">
+                <button type="button">Download Resume</button>
+            </a>
+            
 
         </>
     )
